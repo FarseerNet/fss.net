bbd983acddff6b3a2ef07fde44ebba5c4c90d2e8
niao <qyniao888@es3.com>
2021-05-02 16:19:34 +0800

    添加日志内容

--- 01_UserInterface（前端界面）/FSS.GrpcService/publish.local.sh
+++
@@ -1,4 +0,0 @@
-dotnet publish -c release
-cd bin/release/net5.0
-docker build -t farseernet/fss:1.0.0-beta.3 --network=host .
-docker push farseernet/fss:1.0.0-beta.3
--- pre_script.sh
+++
@@ -1,5 +0,0 @@
-workdir='/root/nfs/git/'
-for dir in $(ls ${workdir});
-do
-    git -C $workdir$dir pull
-done
