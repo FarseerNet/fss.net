9332e85ab82c86126115f1ba0eed6a0c16ef32aa
niao <qyniao888@es3.com>
2021-05-10 02:04:29 +0800

    添加es属性

--- 04_Component（业务组件）/FSS.Com.MetaInfoServer/RunLog/Dal/RunLogPO.cs
+++ 04_Component（业务组件）/FSS.Com.MetaInfoServer/RunLog/Dal/RunLogPO.cs
@@ -1,7 +1,8 @@
 using System;
 using FS.Core.Mapping.Attribute;
 using FS.Mapper;
-using Microsoft.Extensions.Logging;
+using Nest;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace FSS.Com.MetaInfoServer.RunLog.Dal
 {
@@ -14,37 +15,37 @@ namespace FSS.Com.MetaInfoServer.RunLog.Dal
         /// <summary>
         /// 主键
         /// </summary>
-        [Field(Name = "id",IsPrimaryKey = true)]
+        [Field(Name = "id",IsPrimaryKey = true),Number(NumberType.Long)]
         public long? Id { get; set; }
 
         /// <summary>
         /// 任务组记录ID
         /// </summary>
-        [Field(Name = "task_group_id")]
+        [Field(Name = "task_group_id"),Number(NumberType.Integer)]
         public int TaskGroupId { get; set; }
         
         /// <summary>
         /// 任务记录ID
         /// </summary>
-        [Field(Name = "task_id")]
+        [Field(Name = "task_id"),Number(NumberType.Integer)]
         public int TaskId { get; set; }
         
         /// <summary>
         /// 日志级别
         /// </summary>
-        [Field(Name = "log_level")]
+        [Field(Name = "log_level"),Number(NumberType.Byte)]
         public LogLevel LogLevel { get; set; }
         
         /// <summary>
         /// 日志内容
         /// </summary>
-        [Field(Name = "content")]
+        [Field(Name = "content"),Text]
         public string Content { get; set; }
         
         /// <summary>
         /// 日志时间
         /// </summary>
-        [Field(Name = "create_at")]
+        [Field(Name = "create_at"),Date]
         public DateTime CreateAt { get; set; }
     }
 }
