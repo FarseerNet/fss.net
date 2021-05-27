e8b052044d16699c7ff88c5098978aec0f63c691
steden <1470804@qq.com>
2021-05-28 03:26:39 +0800

    修改日志

--- 04_Component（业务组件）/FSS.Com.RemoteCallServer/RemoteCommand/RegisterCommand.cs
+++ 04_Component（业务组件）/FSS.Com.RemoteCallServer/RemoteCommand/RegisterCommand.cs
@@ -55,11 +55,8 @@ namespace FSS.Com.RemoteCallServer.RemoteCommand
             };
             ClientRegister.Register(clientConnectVO);
 
-            IocManager.Logger<RegisterCommand>().LogInformation($"客户端===> {clientConnectVO.ServerHost}、{clientConnectVO.ClientIp} 成功建立连接");
-            await ClientResponse.PrintAsync(_responseStream, $"FSS平台==> {clientConnectVO.ServerHost}：成功建立连接，欢迎{clientConnectVO.ClientIp}");
-            
-            // 有客户端进来，运行调度
-            //await IocManager.Resolve<IWhenTaskStatus>("None").Run();
+            IocManager.Logger<RegisterCommand>().LogInformation($"客户端===> {clientConnectVO.ServerHost}、{clientConnectVO.ClientIp}、{clientConnectVO.JobName} 成功建立连接");
+            await ClientResponse.PrintAsync(_responseStream, $"FSS平台==> {clientConnectVO.ServerHost}：成功建立连接，欢迎{clientConnectVO.ClientIp}、{clientConnectVO.JobName}");
         }
     }
 }
