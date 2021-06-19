3a4750b65388dd528489fc14e5b3110062619e3d
steden <1470804@qq.com>
2021-06-19 11:32:52 +0800

    优化缓存的读写频率

--- 04_Component（业务组件）/FSS.Com.SchedulerServer/Scheduler/WhenTaskStatusNone.cs
+++ 04_Component（业务组件）/FSS.Com.SchedulerServer/Scheduler/WhenTaskStatusNone.cs
@@ -1,19 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FS.Cache.Redis;
 using FS.DI;
 using FS.MQ.RedisStream;
-using FSS.Abstract.Entity.MetaInfo;
 using FSS.Abstract.Enum;
 using FSS.Abstract.Server.MetaInfo;
 using FSS.Abstract.Server.RegisterCenter;
-using FSS.Abstract.Server.RemoteCall;
 using FSS.Abstract.Server.Scheduler;
-using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
 namespace FSS.Com.SchedulerServer.Scheduler
@@ -25,7 +20,6 @@ namespace FSS.Com.SchedulerServer.Scheduler
         public ITaskGroupList     TaskGroupList     { get; set; }
         public ITaskGroupInfo     TaskGroupInfo     { get; set; }
         public ITaskList          TaskList          { get; set; }
-        public ITaskAdd           TaskAdd           { get; set; }
         public ITaskUpdate        TaskUpdate        { get; set; }
         public IIocManager        IocManager        { get; set; }
         public IRunLogAdd         RunLogAdd         { get; set; }
@@ -45,14 +39,14 @@ namespace FSS.Com.SchedulerServer.Scheduler
                     try
                     {
                         var dicTaskGroup = await TaskGroupList.ToListByMemoryAsync();
-                        var lstTask      = await TaskInfo.ToGroupListAsync();
+                        var lstGroupTask      = await TaskInfo.ToGroupListAsync();
 
                         // 找出未执行的任务列表
                         var lstNoneTask = await TaskList.ToNoneListAsync();
                         foreach (var task in lstNoneTask)
                         {
                             // 缓存中，找到相同任务组ID的任务
-                            var taskVO = lstTask.Find(o => o.TaskGroupId == task.TaskGroupId);
+                            var taskVO = lstGroupTask.Find(o => o.TaskGroupId == task.TaskGroupId);
 
                             // 两者的任务ID不一致
                             if (taskVO.Id != task.Id)
@@ -84,7 +78,7 @@ namespace FSS.Com.SchedulerServer.Scheduler
                         //}
 //
                         // 注册进来的客户端，必须是能处理的，否则退出线程
-                        var lstStatusNone = lstTask.FindAll(o => ClientRegister.Exists(o.JobName));
+                        var lstStatusNone = lstGroupTask.FindAll(o => ClientRegister.Exists(o.JobName));
                         if (lstStatusNone == null || lstStatusNone.Count == 0)
                         {
                             await Task.Delay(5000);
@@ -115,8 +109,6 @@ namespace FSS.Com.SchedulerServer.Scheduler
                     catch (Exception e)
                     {
                         logger.LogError(e, e.Message);
-                        // 休眠下，防止CPU过高
-                        await Task.Delay(100);
                     }
 
                     await Task.Delay(5000);
