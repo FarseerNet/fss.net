1d8f8491670b261c306cba84741ee1ae8bc74298
steden <1470804@qq.com>
2022-03-03 20:55:34 +0800

    调整计算平均运行速度的方法

--- 02_Application/FSS.Application/Job/SyncTaskGroupAvgSpeedJob.cs
+++ 02_Application/FSS.Application/Job/SyncTaskGroupAvgSpeedJob.cs
@@ -12,8 +12,7 @@ namespace FSS.Application.Job;
 [FssJob(Name = "FSS.SyncTaskGroupAvgSpeed")]
 public class SyncTaskGroupAvgSpeedJob : IFssJob
 {
-    public TaskGroupService     TaskGroupService    { get; set; }
-    public ITaskGroupRepository TaskGroupRepository { get; set; }
+    public TaskGroupService TaskGroupService { get; set; }
 
     public async Task<bool> Execute(IFssContext context)
     {
@@ -21,8 +20,7 @@ public class SyncTaskGroupAvgSpeedJob : IFssJob
         foreach (var taskGroupVo in taskGroupVos)
         {
             // 先计算在更新
-            var taskGroup = await TaskGroupRepository.ToEntityAsync(taskGroupId: taskGroupVo.Id);
-            await taskGroup.UpdateAvgSpeed();
+            await TaskGroupService.UpdateAvgSpeed(taskGroupVo.Id);
             await Task.Delay(millisecondsDelay: 1000);
         }
         return true;
--- 03_Domain/FSS.Domain.Tasks/TaskGroup/TaskGroupService.cs
+++ 03_Domain/FSS.Domain.Tasks/TaskGroup/TaskGroupService.cs
@@ -9,20 +9,6 @@ public class TaskGroupService : ISingletonDependency
     public ITaskGroupRepository TaskGroupRepository { get; set; }
 
     /// <summary>
-    ///     获取任务组信息
-    /// </summary>
-    public Task<TaskGroupDO> ToEntityAsync(int taskGroupId) => TaskGroupRepository.ToEntityAsync(taskGroupId: taskGroupId);
-
-    /// <summary>
-    ///     删除任务组
-    /// </summary>
-    public async Task DeleteAsync(int taskGroupId)
-    {
-        var taskGroup = await TaskGroupRepository.ToEntityAsync(taskGroupId: taskGroupId);
-        await taskGroup.DeleteAsync();
-    }
-
-    /// <summary>
     ///     获取所有任务组中的任务
     /// </summary>
     public async Task<List<TaskGroupDO>> ToListAsync()
@@ -33,5 +19,22 @@ public class TaskGroupService : ISingletonDependency
             if (taskGroupPO.Task == null)
                 await taskGroupPO.CreateTask();
         return lstTaskGroup;
+    }
+    
+
+    /// <summary>
+    ///     计算平均耗时
+    /// </summary>
+    public async Task UpdateAvgSpeed(int taskGroupId)
+    {
+        var speedList   = await IocManager.GetService<ITaskGroupRepository>().ToTaskSpeedListAsync(taskGroupId: taskGroupId);
+        var runSpeedAvg = new TaskSpeed(speedList: speedList).GetAvgSpeed();
+        
+        if (runSpeedAvg > 0)
+        {
+            var taskGroup = await TaskGroupRepository.ToEntityAsync(taskGroupId);
+            taskGroup.RunSpeedAvg = runSpeedAvg;
+            await IocManager.GetService<ITaskGroupRepository>().SaveAsync(taskGroup);
+        }
     }
 }
